5845667beef6e804dbfe27a9f9f3e111fabc767b
Kaleb5-XO <kaleb.fikre.5@gmail.com>
2021-09-14 13:57:29 +0300

    Second Commit

---
+++ myScript.js
@@ -0,0 +1,84 @@
+// document.getElementById("count_en").innerText=5;
+// 1
+// let age = 30
+// let human2DogAgeRatio = 1/7
+// DogAge = age*1/human2DogAgeRatio
+ 
+// console.log(DogAge)
+
+// 2
+// let bonusPoints = 50
+
+// bonusPoints = bonusPoints * 2
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints / 4
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+// 3
+// function logOut(){
+//     console.log(42)
+// }
+// logOut()
+
+// 4
+// let lap1=34
+// let lap2=33
+// let lap3=36
+
+// function totRaceTime(){
+//     console.log(lap1+lap2+lap3)
+// }
+// totRaceTime()
+
+
+// -----------Subway Passenger Counter--------------
+
+let count = 0
+function Increment(){
+    count++
+    document.getElementById('count_en').textContent = count;
+    console.log(count);
+}
+function Save(){
+    let countStr = " "+count+" - "
+    document.getElementById('Saved').textContent += countStr
+    console.log(count)
+    document.getElementById('count_en').textContent  = 0
+    count = 0 
+}
+
+// let Uname = 'Linda'
+// let greeting = 'Hi there'
+
+// function nameLog(){
+//     console.log(greeting + ", " +  Uname + "!")
+// }
+// nameLog()
+
+// let myPoints = 3
+
+// function add3Points(){
+//     myPoints +=3
+// }
+// function remove1Point(){
+//     myPoints--
+// }
+ 
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+
+// console.log(myPoints)
+
+// 22
+// 18
+// 65
+// My points: 59
+// 4
+// 1114
+
+
